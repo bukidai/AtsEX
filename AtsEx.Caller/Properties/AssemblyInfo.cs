1f76e2364b0fabc25de95eb5c714c8c2c03588e8
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-04-24 00:31:48 +0900

    chore: バージョンを0.6.20424.1に

--- AtsEx.Caller/Properties/AssemblyInfo.cs
+++ AtsEx.Caller/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("AtsEx.Caller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AtsEX Caller")]
+[assembly: AssemblyDescription("AtsEX Caller")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dynabook")]
-[assembly: AssemblyProduct("AtsEx.Caller")]
-[assembly: AssemblyCopyright("Copyright © Dynabook 2022")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AtsEX")]
+[assembly: AssemblyCopyright("Copyright © 2022 automatic9045")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.20424.1")]
--- AtsEx.Core/Properties/AssemblyInfo.cs
+++ AtsEx.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("AtsEx.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AtsEX Core")]
+[assembly: AssemblyDescription("AtsEX Core")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dynabook")]
-[assembly: AssemblyProduct("AtsEx.Core")]
-[assembly: AssemblyCopyright("Copyright © Dynabook 2022")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AtsEX")]
+[assembly: AssemblyCopyright("Copyright © 2022 automatic9045")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.20424.1")]
