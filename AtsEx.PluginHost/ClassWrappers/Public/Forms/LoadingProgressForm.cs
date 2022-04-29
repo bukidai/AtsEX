86986f4765f6f9825112c05c6cd5c01bf22d1834
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-04-29 13:28:42 +0900

    feat: [クラスラッパー]LoadingProgressFormにエラー一覧関連のフィールドを追加

--- AtsEx.PluginHost/ClassWrappers/Public/Forms/LoadingProgressForm.cs
+++ AtsEx.PluginHost/ClassWrappers/Public/Forms/LoadingProgressForm.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 using Automatic9045.AtsEx.PluginHost.BveTypeCollection;
 
@@ -14,6 +15,11 @@ namespace Automatic9045.AtsEx.PluginHost.ClassWrappers
         static LoadingProgressForm()
         {
             ClassMemberCollection members = BveTypeCollectionProvider.Instance.GetClassInfoOf<LoadingProgressForm>();
+
+            IsErrorCriticalField = members.GetSourceFieldOf(nameof(IsErrorCritical));
+            ErrorCountField = members.GetSourceFieldOf(nameof(ErrorCount));
+            PanelField = members.GetSourceFieldOf(nameof(Panel));
+            ErrorListViewField = members.GetSourceFieldOf(nameof(ErrorListView));
 
             ThrowErrorMethod1 = members.GetSourceMethodOf(nameof(ThrowError), new Type[] { typeof(string), typeof(string), typeof(int), typeof(int) });
             ThrowErrorMethod2 = members.GetSourceMethodOf(nameof(ThrowError), new Type[] { typeof(LoadError) });
@@ -29,6 +35,28 @@ namespace Automatic9045.AtsEx.PluginHost.ClassWrappers
             if (src is null) return null;
             return new LoadingProgressForm(src);
         }
+
+
+        private static FieldInfo IsErrorCriticalField;
+        public bool IsErrorCritical
+        {
+            get => IsErrorCriticalField.GetValue(Src);
+            set => IsErrorCriticalField.SetValue(Src, value);
+        }
+
+        private static FieldInfo ErrorCountField;
+        internal int ErrorCount
+        {
+            get => ErrorCountField.GetValue(Src);
+            set => ErrorCountField.SetValue(Src, value);
+        }
+
+        private static FieldInfo PanelField;
+        internal Panel Panel => PanelField.GetValue(Src);
+
+        private static FieldInfo ErrorListViewField;
+        internal ListView ErrorListView => ErrorListViewField.GetValue(Src);
+
 
         private static MethodInfo ThrowErrorMethod1;
         public void ThrowError(string text, string senderFileName, int lineIndex, int charIndex)
