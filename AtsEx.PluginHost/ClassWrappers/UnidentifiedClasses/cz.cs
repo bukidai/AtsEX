df70871e659b3c71b39800724a19442c0f497961
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-05-07 17:40:41 +0900

    feat: ScenarioProvider.Route.Structuresの解放を防止するように
    設置したストラクチャの位置や角度等を取得しやすくするため
    ただし変更してもゲームには適用されず

---
+++ AtsEx.PluginHost/ClassWrappers/UnidentifiedClasses/cz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Automatic9045.AtsEx.PluginHost.BveTypeCollection;
+
+namespace Automatic9045.AtsEx.PluginHost.ClassWrappers
+{
+    [WillRefactor]
+    internal sealed class cz : ClassWrapper
+    {
+        public static class OriginalMemberSet
+        {
+            public static MethodInfo SetRouteMethod { get; internal set; }
+        }
+
+        static cz()
+        {
+            ClassMemberCollection members = BveTypeCollectionProvider.Instance.GetClassInfoOf<cz>();
+
+            OriginalMemberSet.SetRouteMethod = members.GetSourceMethodOf(nameof(SetRoute));
+        }
+
+        private cz(object src) : base(src)
+        {
+        }
+
+        private void SetRoute(Route route)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AtsEx.PluginHost/Helpers/StructureSetLifeProlonger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using Automatic9045.AtsEx.PluginHost.ClassWrappers;
+
+namespace Automatic9045.AtsEx.PluginHost.Helpers
+{
+    internal class StructureSetLifeProlonger
+    {
+        static StructureSetLifeProlonger()
+        {
+            Harmony harmony = new Harmony("com.automatic9045.atsex.classwrappers.structureset-life-prolonger");
+            harmony.Patch(cz.OriginalMemberSet.SetRouteMethod, new HarmonyMethod(typeof(StructureSetLifeProlonger), nameof(SetRoutePrefix)));
+        }
+
+        private static void SetRoutePrefix(object[] __args)
+        {
+            Route route = Route.FromSource(__args[0]);
+            StructureSet structures = route.Structures;
+            InstanceStore.BveHacker.PreviewScenarioProviderCreated += e =>
+            {
+                e.ScenarioProvider.Route.Structures = structures;
+            };
+        }
+    }
+}
