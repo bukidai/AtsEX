2c73ab28d4d1670ac6e765b8296200e1051a15cd
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-07-17 16:32:32 +0900

    feat: [クラスラッパー]AssistantTextDrawerを追加

---
+++ AtsEx.PluginHost/ClassWrappers/Public/AssistantTextDrawer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Automatic9045.AtsEx.PluginHost.BveTypes;
+
+namespace Automatic9045.AtsEx.PluginHost.ClassWrappers
+{
+    /// <summary>
+    /// 補助表示を描画するための機能を提供します。
+    /// </summary>
+    public class AssistantTextDrawer : ClassWrapperBase
+    {
+        [InitializeClassWrapper]
+        private static void Initialize()
+        {
+            ClassMemberSet members = BveTypeSet.Instance.GetClassInfoOf<AssistantTextDrawer>();
+
+            AssistantTextsGetMethod = members.GetSourcePropertyGetterOf(nameof(AssistantTexts));
+        }
+
+        protected AssistantTextDrawer(object src) : base(src)
+        {
+        }
+
+        /// <summary>
+        /// オリジナル オブジェクトからラッパーのインスタンスを生成します。
+        /// </summary>
+        /// <param name="src">ラップするオリジナル オブジェクト。</param>
+        /// <returns>オリジナル オブジェクトをラップした <see cref="AssistantText"/> クラスのインスタンス。</returns>
+        [CreateClassWrapperFromSource]
+        public static AssistantTextDrawer FromSource(object src) => src is null ? null : new AssistantTextDrawer(src);
+
+        protected static MethodInfo AssistantTextsGetMethod;
+        /// <summary>
+        /// 補助表示の一覧を取得します。
+        /// </summary>
+        public WrappedList<AssistantTextBase> AssistantTexts => WrappedList<AssistantTextBase>.FromSource(AssistantTextsGetMethod.Invoke(Src, null));
+    }
+}
