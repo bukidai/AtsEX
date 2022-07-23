f4c82cf4a8de02476aeba70a1cde7cfa753924aa
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-07-24 00:17:53 +0900

    sample: 簡易的なATSのサンプルを追加
    実践的なHandleCommandSetの使用方法を示すため

---
+++ Plugins/VehiclePlugins/AtsEx.VehiclePlugin.SimpleAts/SimpleAts.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Automatic9045.AtsEx.PluginHost;
+using Automatic9045.AtsEx.PluginHost.ClassWrappers;
+using Automatic9045.AtsEx.PluginHost.Handles;
+using Automatic9045.AtsEx.PluginHost.Plugins;
+
+namespace Automatic9045.VehiclePlugins.SimpleAts
+{
+    public class SimpleAts : AssemblyPluginBase
+    {
+        public SimpleAts(PluginBuilder builder) : base(builder, PluginType.VehiclePlugin)
+        {
+        }
+
+        public override HandleCommandSet Tick(TimeSpan elapsed)
+        {
+            UserVehicleLocationManager locationManager = BveHacker.Scenario.LocationManager;
+            AtsEx.PluginHost.Handles.HandleSet handleSet = App.Handles;
+
+            double speedMps = locationManager.SpeedMeterPerSecond;
+
+            if (speedMps > 100d.KmphToMps()) // 100km/h以上出ていたら常用最大ブレーキ
+            {
+                int atsPowerNotch = 0;
+                int atsBrakeNotch = handleSet.Brake.MaxServiceBrakeNotch;
+
+                NotchCommandBase powerCommand = handleSet.Power.GetCommandToSetNotchTo(atsPowerNotch);
+                NotchCommandBase brakeCommand = handleSet.Brake.GetCommandToSetNotchTo(Math.Max(atsBrakeNotch, handleSet.Brake.Notch));
+                ReverserPositionCommandBase reverserCommand = ReverserPositionCommandBase.Continue;
+                ConstantSpeedCommand? constantSpeedCommand = ConstantSpeedCommand.Disable;
+
+                return new HandleCommandSet(powerCommand, brakeCommand, reverserCommand, constantSpeedCommand);
+            }
+            else
+            {
+                return HandleCommandSet.DoNothing;
+            }
+        }
+    }
+}
---
+++ Plugins/VehiclePlugins/AtsEx.VehiclePlugin.SimpleAts/UnitConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatic9045.VehiclePlugins.SimpleAts
+{
+    internal static class UnitConverter
+    {
+        public static double KmphToMps(this double speedKillometerPerHour) => speedKillometerPerHour / 3.6;
+        public static double MpsToKmph(this double speedMeterPerSecond) => speedMeterPerSecond * 3.6;
+    }
+}
