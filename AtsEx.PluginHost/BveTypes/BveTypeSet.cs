62037c2f45d34313958b1c8cccd544f5915a478f
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-08-05 00:10:25 +0900

    docs: BveTypeSetのXMLドキュメントを追加

--- AtsEx.PluginHost/BveTypes/BveTypeSet.cs
+++ AtsEx.PluginHost/BveTypes/BveTypeSet.cs
@@ -10,6 +10,9 @@ using Automatic9045.AtsEx.PluginHost.Resources;
 
 namespace Automatic9045.AtsEx.PluginHost.BveTypes
 {
+    /// <summary>
+    /// クラスラッパーに対応する BVE の型とメンバーの情報を提供します。
+    /// </summary>
     public partial class BveTypeSet
     {
         private static readonly ResourceLocalizer Resources = ResourceLocalizer.FromResXOfType<BveTypeSet>("PluginHost");
@@ -31,12 +34,65 @@ namespace Automatic9045.AtsEx.PluginHost.BveTypes
             OriginalAndWrapperTypes = new SortedList<Type, Type>(types.ToDictionary(type => type.OriginalType, type => type.WrapperType), new TypeComparer());
         }
 
+
+        /// <summary>
+        /// <typeparamref name="TWrapper"/> に指定したラッパー型の情報を取得します。
+        /// </summary>
+        /// <typeparam name="TWrapper">ラッパー型。</typeparam>
+        /// <returns><typeparamref name="TWrapper"/> に指定したラッパー型の情報を表す <see cref="TypeMemberSetBase"/>。</returns>
+        /// <seealso cref="GetClassInfoOf{TWrapper}"/>
+        /// <seealso cref="GetClassInfoOf(Type)"/>
+        /// <seealso cref="GetEnumInfoOf{TWrapper}"/>
+        /// <seealso cref="GetEnumInfoOf(Type)"/>
         public TypeMemberSetBase GetTypeInfoOf<TWrapper>() => Types[typeof(TWrapper)];
+
+        /// <summary>
+        /// <paramref name="wrapperType"/> に指定したラッパー型の情報を取得します。
+        /// </summary>
+        /// <param name="wrapperType">ラッパー型。</param>
+        /// <returns><paramref name="wrapperType"/> に指定したラッパー型の情報を表す <see cref="TypeMemberSetBase"/>。</returns>
+        /// <seealso cref="GetClassInfoOf{TWrapper}"/>
+        /// <seealso cref="GetClassInfoOf(Type)"/>
+        /// <seealso cref="GetEnumInfoOf{TWrapper}"/>
+        /// <seealso cref="GetEnumInfoOf(Type)"/>
         public TypeMemberSetBase GetTypeInfoOf(Type wrapperType) => Types[wrapperType];
+
+
+        /// <summary>
+        /// <typeparamref name="TWrapper"/> に指定したラッパー列挙型の情報を取得します。
+        /// </summary>
+        /// <typeparam name="TWrapper">ラッパー列挙型。</typeparam>
+        /// <returns><typeparamref name="TWrapper"/> に指定したラッパー列挙型の情報を表す <see cref="EnumMemberSet"/>。</returns>
         public EnumMemberSet GetEnumInfoOf<TWrapper>() => (EnumMemberSet)Types[typeof(TWrapper)];
+
+        /// <summary>
+        /// <paramref name="wrapperType"/> に指定したラッパー列挙型の情報を取得します。
+        /// </summary>
+        /// <param name="wrapperType">ラッパー列挙型。</param>
+        /// <returns><paramref name="wrapperType"/> に指定したラッパー列挙型の情報を表す <see cref="EnumMemberSet"/>。</returns>
         public EnumMemberSet GetEnumInfoOf(Type wrapperType) => (EnumMemberSet)Types[wrapperType];
+
+
+        /// <summary>
+        /// <typeparamref name="TWrapper"/> に指定したラッパークラスの情報を取得します。
+        /// </summary>
+        /// <typeparam name="TWrapper">ラッパークラス。</typeparam>
+        /// <returns><typeparamref name="TWrapper"/> に指定したラッパークラスの情報を表す <see cref="ClassMemberSet"/>。</returns>
         public ClassMemberSet GetClassInfoOf<TWrapper>() => (ClassMemberSet)Types[typeof(TWrapper)];
+
+        /// <summary>
+        /// <paramref name="wrapperType"/> に指定したラッパークラスの情報を取得します。
+        /// </summary>
+        /// <param name="wrapperType">ラッパークラス。</param>
+        /// <returns><paramref name="wrapperType"/> に指定したラッパークラスの情報を表す <see cref="ClassMemberSet"/>。</returns>
         public ClassMemberSet GetClassInfoOf(Type wrapperType) => (ClassMemberSet)Types[wrapperType];
+
+
+        /// <summary>
+        /// <paramref name="originalType"/> に指定したオリジナル型のラッパー型を取得します。
+        /// </summary>
+        /// <param name="originalType">オリジナル型。</param>
+        /// <returns></returns>
         public Type GetWrapperTypeOf(Type originalType) => OriginalAndWrapperTypes[originalType];
 
         public void Dispose()
