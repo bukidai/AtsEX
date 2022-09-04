242e42fcb6550684baa578d5d8cec65a7037c1b9
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-09-04 11:41:16 +0900

    docs: VehicleSpec、VehicleStateのXMLドキュメントを追加

--- AtsEx.PluginHost/Imports/VehicleSpec.cs
+++ AtsEx.PluginHost/Imports/VehicleSpec.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace Automatic9045.AtsEx.PluginHost
 {
+    /// <summary>
+    /// 車両諸元情報を提供します。
+    /// </summary>
     public class VehicleSpec
     {
+        /// <summary>
+        /// <see cref="VehicleSpec"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="brakeNotches">ブレーキノッチ数。</param>
+        /// <param name="powerNotches">力行ノッチ数。</param>
+        /// <param name="atsNotch">ATS 確認ノッチ。</param>
+        /// <param name="b67notch">ブレーキ弁 67 度に相当するノッチ。</param>
+        /// <param name="cars">編成両数。</param>
         public VehicleSpec(int brakeNotches, int powerNotches, int atsNotch, int b67notch, int cars)
         {
             BrakeNotches = brakeNotches;
@@ -17,10 +28,29 @@ namespace Automatic9045.AtsEx.PluginHost
             Cars = cars;
         }
 
+        /// <summary>
+        /// ブレーキノッチ数を取得します。
+        /// </summary>
         public int BrakeNotches { get; }
+
+        /// <summary>
+        /// 力行ノッチ数を取得します。
+        /// </summary>
         public int PowerNotches { get; }
+
+        /// <summary>
+        /// ATS 確認ノッチを取得します。
+        /// </summary>
         public int AtsNotch { get; }
+
+        /// <summary>
+        /// ブレーキ弁 67 度に相当するノッチを取得します。
+        /// </summary>
         public int B67Notch { get; }
+
+        /// <summary>
+        /// 編成両数を取得します。
+        /// </summary>
         public int Cars { get; }
     }
 }
--- AtsEx.PluginHost/Imports/VehicleState.cs
+++ AtsEx.PluginHost/Imports/VehicleState.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace Automatic9045.AtsEx.PluginHost
 {
+    /// <summary>
+    /// 車両の状態量を提供します。
+    /// </summary>
     public class VehicleState
     {
+        /// <summary>
+        /// <see cref="VehicleState"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="location">列車位置 [m]。</param>
+        /// <param name="speed">列車速度 [km/h]。</param>
+        /// <param name="time">現在時刻。</param>
+        /// <param name="bcPressure">ブレーキシリンダ圧力 [kPa]。</param>
+        /// <param name="mrPressure">元空気ダメ圧力 [kPa]。</param>
+        /// <param name="erPressure">釣り合い空気ダメ圧力 [kPa]。</param>
+        /// <param name="bpPressure">釣り合い空気ダメ圧力 [kPa]。</param>
+        /// <param name="sapPressure">直通管圧力 [kPa]。</param>
+        /// <param name="current">電流 [A]。</param>
         public VehicleState(double location, float speed, TimeSpan time, float bcPressure, float mrPressure, float erPressure, float bpPressure, float sapPressure, float current)
         {
             Location = location;
@@ -22,15 +37,51 @@ namespace Automatic9045.AtsEx.PluginHost
             Current = current;
         }
 
+
+        /// <summary>
+        /// 列車位置 [m] を取得します。
+        /// </summary>
         public double Location { get; }
+
+        /// <summary>
+        /// 列車速度 [km/h] を取得します。
+        /// </summary>
         public float Speed { get; }
+
+        /// <summary>
+        /// 現在時刻を取得します。
+        /// </summary>
         public TimeSpan Time { get; }
 
+
+        /// <summary>
+        /// ブレーキシリンダ圧力 [kPa] を取得します。
+        /// </summary>
         public float BcPressure { get; }
+
+        /// <summary>
+        /// 元空気ダメ圧力 [kPa] を取得します。
+        /// </summary>
         public float MrPressure { get; }
+
+        /// <summary>
+        /// 釣り合い空気ダメ圧力 [kPa] を取得します。
+        /// </summary>
         public float ErPressure { get; }
+
+        /// <summary>
+        /// ブレーキ管圧力 [kPa] を取得します。
+        /// </summary>
         public float BpPressure { get; }
+
+        /// <summary>
+        /// 直通管圧力 [kPa] を取得します。
+        /// </summary>
         public float SapPressure { get; }
+
+        /// <summary>
+        /// 電流 [A] を取得します。
+        /// </summary>
         public float Current { get; }
     }
 }
