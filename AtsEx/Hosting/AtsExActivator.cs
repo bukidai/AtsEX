b0027e0e027befdc3232b4f25e0adcbdd6fe5b18
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-09-10 22:44:12 +0900

    chore: 不必要なHarmonyのアセンブリ解決処理を削除

--- AtsEx/Hosting/AtsExActivator.cs
+++ AtsEx/Hosting/AtsExActivator.cs
@@ -63,7 +63,6 @@ namespace Automatic9045.AtsEx.Hosting
         public void ResolveAssemblies()
         {
             AssemblyResolver.Register(Path.Combine(DirectoryName, "AtsEx.PluginHost.dll"));
-            AssemblyResolver.Register(Path.Combine(DirectoryName, "0Harmony.dll"));
         }
 
         private void CheckAssembly()
