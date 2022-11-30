964632836b2c343ea61dd4ac4a631db5c18fa936
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-12-01 01:10:36 +0900

    feat: BveFileLoadExceptionを、innerExceptionを使って初期化できるように

--- AtsEx.PluginHost/Exceptions/BveFileLoadException.cs
+++ AtsEx.PluginHost/Exceptions/BveFileLoadException.cs
@@ -12,6 +12,25 @@ namespace AtsEx.PluginHost
         public int LineIndex { get; } = 0;
         public int CharIndex { get; } = 0;
 
+        public BveFileLoadException(Exception innerException) : base(innerException.Message, innerException)
+        {
+        }
+
+        public BveFileLoadException(Exception innerException, string senderFileName) : this(innerException)
+        {
+            SenderFileName = senderFileName;
+        }
+
+        public BveFileLoadException(Exception innerException, string senderFileName, int lineIndex) : this(innerException, senderFileName)
+        {
+            LineIndex = lineIndex;
+        }
+
+        public BveFileLoadException(Exception innerException, string senderFileName, int lineIndex, int charIndex) : this(innerException, senderFileName, lineIndex)
+        {
+            CharIndex = charIndex;
+        }
+
         public BveFileLoadException(string message) : base(message)
         {
         }
