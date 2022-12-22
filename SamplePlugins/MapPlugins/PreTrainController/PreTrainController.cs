52969e830f296d5a71da0f5c0fb9ef673cb70806
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-12-23 02:02:40 +0900

    sample: 先行列車を操作するサンプルを追加

---
+++ SamplePlugins/MapPlugins/PreTrainController/PreTrainController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BveTypes.ClassWrappers;
+
+using AtsEx.PluginHost;
+using AtsEx.PluginHost.Plugins;
+
+using AtsEx.Extensions.PreTrainPatch;
+
+namespace AtsEx.Samples.MapPlugins.PreTrainController
+{
+    [PluginType(PluginType.MapPlugin)]
+    public class PreTrainController : AssemblyPluginBase, IDisposable
+    {
+        private Train Train;
+        private PreTrainPatch PreTrainPatch;
+
+        public PreTrainController(PluginBuilder builder) : base(builder)
+        {
+            BveHacker.ScenarioCreated += e =>
+            {
+                if (!e.Scenario.Trains.ContainsKey("test"))
+                {
+                    throw new BveFileLoadException("キーが 'test' の他列車が見つかりませんでした。", "PreTrainController");
+                }
+
+                Train = e.Scenario.Trains["test"];
+                Train.TrainInfo.TrackKey = "0";
+                Train.Location = 35;
+
+                SectionManager sectionManager = e.Scenario.SectionManager;
+                PreTrainPatch = Extensions.GetExtension<IPreTrainPatchFactory>().Patch(sectionManager, new PreTrainLocationConverter(Train, sectionManager));
+            };
+        }
+
+        public override void Dispose()
+        {
+            PreTrainPatch?.Dispose();
+        }
+
+        public override TickResult Tick(TimeSpan elapsed)
+        {
+            if (Train.Location < 15.1)
+            {
+                Train.Location = 15.1;
+            }
+            else if (1520.1 < Train.Location)
+            {
+                Train.Location = 1520.1;
+            }
+
+            return new MapPluginTickResult();
+        }
+
+
+        private class PreTrainLocationConverter : IPreTrainLocationConverter
+        {
+            private readonly Train SourceTrain;
+            private readonly SectionManager SectionManager;
+
+            public PreTrainLocationConverter(Train sourceTrain, SectionManager sectionManager)
+            {
+                SourceTrain = sourceTrain;
+                SectionManager = sectionManager;
+            }
+
+            public PreTrainLocation Convert(PreTrainLocation source)
+                => SourceTrain.TrainInfo.TrackKey == "0" ? PreTrainLocation.FromLocation(SourceTrain.Location, SectionManager) : source;
+        }
+    }
+}
