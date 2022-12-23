96ecdad4e2c0e9b1ff03cb1ef61da90d434c9606
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-12-24 01:19:23 +0900

    sample: 特定閉塞の信号現示を操作するサンプルを追加

---
+++ SamplePlugins/MapPlugins/SignalController/SignalController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BveTypes.ClassWrappers;
+
+using AtsEx.PluginHost.Input.Native;
+using AtsEx.PluginHost.Plugins;
+
+using AtsEx.Extensions.SignalPatch;
+
+namespace AtsEx.Samples.MapPlugins.SignalController
+{
+    [PluginType(PluginType.MapPlugin)]
+    public class SignalController : AssemblyPluginBase, IDisposable
+    {
+        private SignalPatch SignalPatch;
+        private int SignalIndex = 0;
+
+        public SignalController(PluginBuilder builder) : base(builder)
+        {
+            BveHacker.ScenarioCreated += e =>
+            {
+                SectionManager sectionManager = e.Scenario.SectionManager;
+                Section section = sectionManager.Sections[2] as Section;
+
+                SignalPatch = Extensions.GetExtension<ISignalPatchFactory>().Patch(sectionManager, section, source => SignalIndex);
+            };
+
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.D].Pressed += OnDPressed;
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.E].Pressed += OnEPressed;
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.F].Pressed += OnFPressed;
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.G].Pressed += OnGPressed;
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.H].Pressed += OnHPressed;
+        }
+
+        private void OnDPressed(object sender, EventArgs e) => SignalIndex = 0;
+        private void OnEPressed(object sender, EventArgs e) => SignalIndex = 1;
+        private void OnFPressed(object sender, EventArgs e) => SignalIndex = 2;
+        private void OnGPressed(object sender, EventArgs e) => SignalIndex = 3;
+        private void OnHPressed(object sender, EventArgs e) => SignalIndex = 4;
+
+        public override void Dispose()
+        {
+            SignalPatch?.Dispose();
+
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.D].Pressed -= OnDPressed;
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.E].Pressed -= OnEPressed;
+        }
+
+        public override TickResult Tick(TimeSpan elapsed) => new MapPluginTickResult();
+    }
+}
