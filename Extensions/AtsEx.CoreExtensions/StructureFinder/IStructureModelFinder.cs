3515fd4dfba9167158957cd71749f71f6878345f
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-01-23 23:46:47 +0900

    feat: [コア拡張機能]3Dモデルからストラクチャーキーを検索する機能を追加

---
+++ Extensions/AtsEx.CoreExtensions/StructureFinder/IStructureModelFinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BveTypes.ClassWrappers;
+
+using AtsEx.PluginHost.Plugins.Extensions;
+
+namespace AtsEx.Extensions.StructureModelFinder
+{
+    /// <summary>
+    /// ストラクチャーの 3D モデルを簡単に検索するための機能を提供します。
+    /// </summary>
+    public interface IStructureModelFinder : IExtension
+    {
+        /// <summary>
+        /// ストラクチャーキーから 3D モデルを検索します。
+        /// </summary>
+        /// <param name="structureKey">ストラクチャーキー。</param>
+        /// <returns><paramref name="structureKey"/> に対応する 3D モデル。</returns>
+        Model GetModel(string structureKey);
+
+        /// <summary>
+        /// 3D モデルからストラクチャーキーを検索します。
+        /// </summary>
+        /// <param name="model">3D モデル。</param>
+        /// <returns><paramref name="model"/> に対応するストラクチャーキー。</returns>
+        string GetStructureKey(Model model);
+    }
+}
---
+++ Extensions/AtsEx.CoreExtensions/StructureFinder/StructureModelFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BveTypes.ClassWrappers;
+using BveTypes.ClassWrappers.Extensions;
+
+using AtsEx.PluginHost;
+using AtsEx.PluginHost.Plugins;
+using AtsEx.PluginHost.Plugins.Extensions;
+
+namespace AtsEx.Extensions.StructureModelFinder
+{
+    [PluginType(PluginType.Extension)]
+    [ExtensionMainDisplayType(typeof(IStructureModelFinder))]
+    internal class StructureModelFinder : AssemblyPluginBase, IStructureModelFinder
+    {
+        private WrappedSortedList<string, Model> KeyToModel;
+        private Dictionary<Model, string> ModelToKey;
+
+        public override string Title { get; } = nameof(StructureModelFinder);
+        public override string Description { get; } = "ストラクチャーの 3D モデルを簡単に検索するための機能を提供します。";
+
+        public StructureModelFinder(PluginBuilder builder) : base(builder)
+        {
+            BveHacker.ScenarioCreated += OnScenarioCreated;
+        }
+
+        private void OnScenarioCreated(ScenarioCreatedEventArgs e)
+        {
+            WrappedSortedList<string, Model> loadedModels = e.Scenario.Route.StructureModels;
+
+            KeyToModel = loadedModels;
+            ModelToKey = loadedModels.Where(item => !(item.Value is null)).ToDictionary(item => item.Value, item => item.Key);
+        }
+
+        public override void Dispose()
+        {
+            BveHacker.ScenarioCreated -= OnScenarioCreated;
+        }
+
+        public override TickResult Tick(TimeSpan elapsed) => new ExtensionTickResult();
+
+        public Model GetModel(string structureKey) => KeyToModel[structureKey];
+        public string GetStructureKey(Model model) => ModelToKey[model];
+    }
+}
