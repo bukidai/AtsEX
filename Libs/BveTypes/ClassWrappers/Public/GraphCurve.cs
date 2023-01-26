37316e152658d7e549f0b4cca8d98ebf206d10f9
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-01-26 23:03:38 +0900

    feat: [クラスラッパー]GraphCurveをList<GraphCurvePoint>として実装し、編集可能に

--- Libs/BveTypes/ClassWrappers/Public/GraphCurve.cs
+++ Libs/BveTypes/ClassWrappers/Public/GraphCurve.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +8,14 @@ using System.Threading.Tasks;
 using FastMember;
 using TypeWrapping;
 
+using BveTypes.ClassWrappers.Extensions;
+
 namespace BveTypes.ClassWrappers
 {
     /// <summary>
     /// xy 平面上の、x 座標からそれに対応する y 座標を取得可能なグラフを表します。
     /// </summary>
-    public class GraphCurve : ClassWrapperBase
+    public class GraphCurve : WrappedList<GraphCurvePoint>
     {
         [InitializeClassWrapper]
         private static void Initialize(BveTypeSet bveTypes)
@@ -26,7 +29,7 @@ namespace BveTypes.ClassWrappers
         /// オリジナル オブジェクトから <see cref="GraphCurve"/> クラスの新しいインスタンスを初期化します。
         /// </summary>
         /// <param name="src">ラップするオリジナル オブジェクト。</param>
-        protected GraphCurve(object src) : base(src)
+        protected GraphCurve(IList src) : base(src)
         {
         }
 
@@ -36,7 +39,7 @@ namespace BveTypes.ClassWrappers
         /// <param name="src">ラップするオリジナル オブジェクト。</param>
         /// <returns>オリジナル オブジェクトをラップした <see cref="GraphCurve"/> クラスのインスタンス。</returns>
         [CreateClassWrapperFromSource]
-        public static GraphCurve FromSource(object src) => src is null ? null : new GraphCurve(src);
+        public static GraphCurve FromSource(object src) => src is null ? null : new GraphCurve((IList)src);
 
         private static FastMethod GetYMethod;
         /// <summary>
@@ -44,6 +47,6 @@ namespace BveTypes.ClassWrappers
         /// </summary>
         /// <param name="x"></param>
         /// <returns></returns>
-        public double GetY(double x) => GetYMethod.Invoke(Src, new object[] { x });
+        public double GetY(double x) => (double)GetYMethod.Invoke(Src, new object[] { x });
     }
 }
---
+++ Libs/BveTypes/ClassWrappers/Public/GraphCurvePoint.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FastMember;
+using TypeWrapping;
+
+namespace BveTypes.ClassWrappers
+{
+    /// <summary>
+    /// <see cref="GraphCurve"/> で線形補間処理の基となる点を表します。
+    /// </summary>
+    public class GraphCurvePoint : ClassWrapperBase
+    {
+        private static Type OriginalType;
+
+        [InitializeClassWrapper]
+        private static void Initialize(BveTypeSet bveTypes)
+        {
+            ClassMemberSet members = bveTypes.GetClassInfoOf<GraphCurvePoint>();
+
+            OriginalType = members.OriginalType;
+
+            XGetMethod = members.GetSourcePropertyGetterOf(nameof(X));
+
+            YGetMethod = members.GetSourcePropertyGetterOf(nameof(Y));
+        }
+
+        /// <summary>
+        /// オリジナル オブジェクトから <see cref="GraphCurvePoint"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="src">ラップするオリジナル オブジェクト。</param>
+        protected GraphCurvePoint(object src) : base(src)
+        {
+        }
+
+        /// <summary>
+        /// オリジナル オブジェクトからラッパーのインスタンスを生成します。
+        /// </summary>
+        /// <param name="src">ラップするオリジナル オブジェクト。</param>
+        /// <returns>オリジナル オブジェクトをラップした <see cref="GraphCurvePoint"/> クラスのインスタンス。</returns>
+        [CreateClassWrapperFromSource]
+        public static GraphCurvePoint FromSource(object src) => src is null ? null : new GraphCurvePoint(src);
+
+        /// <summary>
+        /// 座標を指定して、<see cref="GraphCurvePoint"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="x">x 座標。</param>
+        /// <param name="y">y 座標。</param>
+        public GraphCurvePoint(double x, double y) : this(Activator.CreateInstance(OriginalType, x, y))
+        {
+        }
+
+        private static FastMethod XGetMethod;
+        /// <summary>
+        /// x 座標を取得します。
+        /// </summary>
+        public double X => XGetMethod.Invoke(Src, null);
+
+        private static FastMethod YGetMethod;
+        /// <summary>
+        /// y 座標を取得します。
+        /// </summary>
+        public double Y => YGetMethod.Invoke(Src, null);
+    }
+}
