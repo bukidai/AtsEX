6d4575fefc71f0255e82c4c4f65f28cff34053c6
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-02-02 00:27:51 +0900

    sample: [SimpleAts]ATS作動時、ATSサウンドを再生するように

--- SamplePlugins/VehiclePlugins/SimpleAts/SimpleAts.cs
+++ SamplePlugins/VehiclePlugins/SimpleAts/SimpleAts.cs
@@ -10,14 +10,19 @@ using BveTypes.ClassWrappers;
 using AtsEx.PluginHost;
 using AtsEx.PluginHost.Handles;
 using AtsEx.PluginHost.Plugins;
+using AtsEx.PluginHost.Sound;
+using AtsEx.PluginHost.Sound.Native;
 
 namespace AtsEx.Samples.VehiclePlugins.SimpleAts
 {
     [PluginType(PluginType.VehiclePlugin)]
     public class SimpleAts : AssemblyPluginBase
     {
+        private readonly IAtsSound AtsSound;
+
         public SimpleAts(PluginBuilder builder) : base(builder)
         {
+            AtsSound = Native.AtsSounds.Register(0);
         }
 
         public override void Dispose()
@@ -34,6 +39,8 @@ namespace AtsEx.Samples.VehiclePlugins.SimpleAts
             VehiclePluginTickResult tickResult = new VehiclePluginTickResult();
             if (speedMps > 100d.KmphToMps()) // 100km/h以上出ていたら常用最大ブレーキ
             {
+                if (AtsSound.PlayState == PlayState.Stop) AtsSound.PlayLoop();
+
                 int atsPowerNotch = 0;
                 int atsBrakeNotch = handleSet.Brake.MaxServiceBrakeNotch;
 
@@ -43,6 +50,10 @@ namespace AtsEx.Samples.VehiclePlugins.SimpleAts
                 ConstantSpeedCommand? constantSpeedCommand = ConstantSpeedCommand.Disable;
 
                 tickResult.HandleCommandSet = new HandleCommandSet(powerCommand, brakeCommand, reverserCommand, constantSpeedCommand);
+            }
+            else
+            {
+                if (AtsSound.PlayState == PlayState.PlayingLoop) AtsSound.Stop();
             }
 
             return tickResult;
