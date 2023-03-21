1dc72ef1b3a0ed965158b07c7dd6a8e7ae06e05c
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-03-21 22:52:22 +0900

    sample: マップPIから車両PIを参照するサンプルを追加

---
+++ SamplePlugins/MapPlugins/CooperatingWithVehiclePlugin/PluginMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AtsEx.PluginHost.Plugins;
+
+using VehiclePlugin = AtsEx.Samples.VehiclePlugins.CooperatingWithMapPlugin.PluginMain;
+
+namespace AtsEx.Samples.MapPlugins.CooperatingWithVehiclePlugin
+{
+    [PluginType(PluginType.MapPlugin)]
+    public class PluginMain : AssemblyPluginBase
+    {
+        public PluginMain(PluginBuilder builder) : base(builder)
+        {
+            Plugins.AllPluginsLoaded += OnAllPluginsLoaded;
+        }
+
+        private void OnAllPluginsLoaded(object sender, EventArgs e)
+        {
+            VehiclePlugin vehiclePlugin = Plugins[PluginType.VehiclePlugin]["TestPlugin"] as VehiclePlugin;
+            MessageBox.Show($"車両プラグインから値を取得しました: {vehiclePlugin.SharedValue}", "AtsEX マッププラグイン：車両プラグイン連携サンプル");
+        }
+
+        public override void Dispose()
+        {
+            Plugins.AllPluginsLoaded -= OnAllPluginsLoaded;
+        }
+
+        public override TickResult Tick(TimeSpan elapsed) => new MapPluginTickResult();
+    }
+}
---
+++ SamplePlugins/VehiclePlugins/CooperatingWithMapPlugin/PluginMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AtsEx.PluginHost.Plugins;
+
+namespace AtsEx.Samples.VehiclePlugins.CooperatingWithMapPlugin
+{
+    [PluginType(PluginType.VehiclePlugin)]
+    public class PluginMain : AssemblyPluginBase
+    {
+        public int SharedValue { get; } = 123;
+
+        public PluginMain(PluginBuilder builder) : base(builder)
+        {
+        }
+
+        public override void Dispose()
+        {
+        }
+
+        public override TickResult Tick(TimeSpan elapsed) => new VehiclePluginTickResult();
+    }
+}
