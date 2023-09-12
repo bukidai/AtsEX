4737b376000f30b4ceed5724add72d448a1122ae
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-09-13 02:38:01 +0900

    feat: [クラスラッパー]音声の設置位置、速度を設定できるように

--- Libs/BveTypes/ClassWrappers/Public/Sound.cs
+++ Libs/BveTypes/ClassWrappers/Public/Sound.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SlimDX;
 using SlimDX.DirectSound;
 
 using FastMember;
@@ -23,6 +24,12 @@ namespace BveTypes.ClassWrappers
 
             Constructor1 = members.GetSourceConstructor(new Type[] { typeof(TimeManager), typeof(CameraLocation), typeof(SecondarySoundBuffer), typeof(double), typeof(SoundPosition) });
             Constructor2 = members.GetSourceConstructor(new Type[] { typeof(TimeManager), typeof(CameraLocation), typeof(SecondarySoundBuffer[]), typeof(double), typeof(SoundPosition) });
+
+            PositionInBlockGetMethod = members.GetSourcePropertyGetterOf(nameof(PositionInBlock));
+            PositionInBlockSetMethod = members.GetSourcePropertySetterOf(nameof(PositionInBlock));
+
+            SpeedInBlockGetMethod = members.GetSourcePropertyGetterOf(nameof(SpeedInBlock));
+            SpeedInBlockSetMethod = members.GetSourcePropertySetterOf(nameof(SpeedInBlock));
 
             PlayMethod1 = members.GetSourceMethodOf(nameof(Play), new Type[] { typeof(double), typeof(double), typeof(int) });
             PlayMethod2 = members.GetSourceMethodOf(nameof(Play), new Type[] { typeof(double), typeof(double), typeof(int), typeof(int) });
@@ -56,6 +63,28 @@ namespace BveTypes.ClassWrappers
         public Sound(TimeManager timeManager, CameraLocation cameraLocation, SecondarySoundBuffer[] buffers, double minRadius, SoundPosition position)
             : this(Constructor2.Invoke(new object[] { timeManager, cameraLocation, buffers, minRadius, position }))
         {
+        }
+
+        private static FastMethod PositionInBlockGetMethod;
+        private static FastMethod PositionInBlockSetMethod;
+        /// <summary>
+        /// 現在自列車が走行しているマップ ブロックの原点に対する、この音声の再生位置の位置ベクトル [m] を取得します。
+        /// </summary>
+        public Vector3 PositionInBlock
+        {
+            get => PositionInBlockGetMethod.Invoke(Src, null);
+            set => PositionInBlockSetMethod.Invoke(Src, new object[] { value });
+        }
+
+        private static FastMethod SpeedInBlockGetMethod;
+        private static FastMethod SpeedInBlockSetMethod;
+        /// <summary>
+        /// 現在自列車が走行しているマップ ブロックに対する、この音声の速度ベクトル [m/s] を取得します。
+        /// </summary>
+        public Vector3 SpeedInBlock
+        {
+            get => SpeedInBlockGetMethod.Invoke(Src, null);
+            set => SpeedInBlockSetMethod.Invoke(Src, new object[] { value });
         }
 
         private static FastMethod PlayMethod1;
