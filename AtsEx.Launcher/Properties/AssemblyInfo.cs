83b32550586ae8c8b6e2c31b8cdca15d0f62b298
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-11-18 17:42:41 +0900

    chore: バージョンを1.0.31118.2に
    最新版であってもアップデート情報ダイアログが表示されてしまうため、再更新

--- AtsEx.Launcher/Properties/AssemblyInfo.cs
+++ AtsEx.Launcher/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using AtsEx.Launcher;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.31106.1")]
+[assembly: AssemblyVersion("1.0.31118.2")]
 
 // 互換性バージョンの履歴
 // ==============================
